9a0c969ccdad41d535c4cf61dd5a11243e30844f
midunodunsi <midun99.odunsi@gmail.com>
2024-06-08 17:41:29 +0100

    state.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -3,8 +3,7 @@
 __init__ for app_views
 """
 from flask import Blueprint
-
-app_views = Blueprint('/api/v1', __name__, url_prefix='/api/v1')
-
 from api.v1.views.index import *
 from api.v1.views.states import *
+
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
